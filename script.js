8b9d18684feacfe31dcebe099de7af15e29f56c7
Sultanito <sulter731@gmail.com>
2024-02-29 15:41:30 +0600

    feb 29 2024

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const calorieCounter = document.getElementById('calorie-counter');
-const entryDropdown = document.getElementById('entry-dropdown')
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
+let isError = false;
+
+function cleanInputString(str) {
+  const regex = /\+-\s/;
+}
